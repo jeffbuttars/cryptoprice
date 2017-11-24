b4243a86a640bdf0e0bd6b304b01e66007bfe23f
Jeff Buttars <jeffbuttars@gmail.com>
2017-11-24 10:46:10 -0700

    init

---
+++ SlackCryptoPriceBot/bot.py
@@ -0,0 +1,15 @@
+class Bot(object):
+
+    def __init__(self, name, emoji, oauth, verification, client, messages):
+        self._name = name
+        self._emoji = emoji
+        self._oauth = oauth
+        self._verification = verification
+        self._client = client
+        self._messages = messages
+
+    def auth(self, arg1):
+        pass
+
+    def open_dm(self, arg1):
+        pass
