a2ec93f0986b6d421ec688ad30b94bb4df60cd9b
Jeff Buttars <jeffbuttars@gmail.com>
2017-11-26 12:56:05 -0700

    WIP

--- cryptoprice/app.py
+++ cryptoprice/app.py
@@ -1,21 +1,65 @@
-from apistar import Include, Route
+#!/usr/bin/env python
+# encoding: utf-8
+
+from pprint import pformat as pf
+from apistar import Include
 from apistar.frameworks.asyncio import ASyncIOApp as App
 from apistar.handlers import docs_urls, static_urls
+from index.index import routes as index_routes
+import logging
+
+# Set up the logger
+logger = logging.getLogger(__name__)
+# Use a console handler, set it to debug by default
+logger_ch = logging.StreamHandler()
+logger.setLevel(logging.DEBUG)
+#  log_formatter = logging.Formatter(('%(levelname)s: %(asctime)s %(processName)s:%(process)d'
+#                                     ' %(filename)s:%(lineno)s %(module)s::%(funcName)s()'
+#                                     ' -- %(message)s'))
+#  logger_ch.setFormatter(log_formatter)
+logger.addHandler(logger_ch)
 
 
-def welcome(name=None):
-    if name is None:
-        return {'message': 'Welcome to API Star!'}
-    return {'message': 'Welcome to API Star, %s!' % name}
+#  async def welcome(
+#      username: str='',
+#      request: http.Request=None,
+#      params: http.QueryParams={},
+#      headers: http.Headers={},
+#  ):
+#      rstr = '%s : %s' % (request, dir(request))
+#      pstr = '%s' % (params,)
+#      hstr = '%s' % (headers,)
 
+#      logger.debug("Request: %s", rstr)
+#      logger.debug("Params: %s", pstr)
+#      logger.debug("Headers: %s", hstr)
+
+#      if username is None:
+#          return {
+#              'message': 'Welcome to API Star!',
+#              'request': rstr,
+#          }
+
+#      return {
+#          'message': 'Welcome to API Star, %s!' % username,
+#          'request': rstr,
+#      }
 
 routes = [
-    Route('/', 'GET', welcome),
+    Include('/', index_routes),
+    Include('/static', static_urls),
     Include('/docs', docs_urls),
-    Include('/static', static_urls)
 ]
 
-app = App(routes=routes)
+settings = {
+    'TEMPLATES': {
+        'ROOT_DIR': ['index/templates'],
+        'PACKAGE_DIRS': ['apistar'],
+    },
+}
+logger.debug("settings %s", pf(settings))
+
+app = App(routes=routes, settings=settings)
 
 
 if __name__ == '__main__':
